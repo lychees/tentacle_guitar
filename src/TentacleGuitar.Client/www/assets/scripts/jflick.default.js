09f75e71ae6b3dc06ffefa8861aad78737acb827
あまみや ゆうこ <1@1234.sh>
2016-06-16 08:08:27 +0800

    Added artworks

---
+++ src/TentacleGuitar.Client/www/assets/scripts/jflick.default.js
@@ -0,0 +1,32 @@
+﻿router.global.popping(function (req, top, bottom, next, final) {
+    if (next)
+        next();
+    if (final)
+        final();
+    return;
+});
+
+router.global.popped(function (req, top, bottom, next) {
+    top.addClass('hidden');
+    setTimeout(function () { top.remove(); }, 300);
+    bottom.removeClass('hidden');
+    if (next)
+        next();
+});
+
+router.global.loading(function (req, top, bottom, next, final) {
+    if (next)
+        next();
+    final();
+});
+
+router.global.loaded(function (req, top, bottom, next) {
+    top.appendTo('body');
+    setTimeout(function () {
+        top.removeClass('hidden');
+        bottom.addClass('hidden');
+
+        if (next)
+            next();
+    }, 50);
+});
